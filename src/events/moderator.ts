66e9e9dc37f23b6d42a65e11879a7c578e10d5a5
fanman1 <59294394+fanman1@users.noreply.github.com>
2021-09-09 14:47:43 -0400

    changes to moderator

--- src/events/moderator.ts
+++ src/events/moderator.ts
@@ -2,6 +2,7 @@ import * as Discord from "discord.js";
 import { IBotEvent } from "../api/eapi";
 var fs = require('fs');
 import * as db from "quick.db";
+var badwordsArray = require('badwords/array'); //npm install badwords
 var forbiddenWords=['banana','apple','strawberry', 'pineapple','grape','melon','peach','avacado','mango'];//use fruit names for demonstration purposes
 export default class moderator implements IBotEvent {
 
@@ -16,36 +17,34 @@ export default class moderator implements IBotEvent {
     
     async runEvent(msg: Discord.Message, Bot: Discord.Client): Promise<void> {
         let arr=db.get(`${msg.author.id}.messages`)
-        console.log(db.all())
+        //console.log(badwordsArray)
         let allMessages: string = arr.join('');
         
-        
-        for(var i=0;i<forbiddenWords.length;i++){
-          if (allMessages.includes(forbiddenWords[i])) {
+        //allMessages.includes(forbiddenWords[i])
+        for(var i=0;i<badwordsArray.length;i++){
+            //console.log('im in the for loop')
+          if (allMessages.includes(badwordsArray[i])) {
+              //console.log('im in the if statement')
             //const mentionedUser = msg.mentions.users.first();
             db.add(`${msg.author.id}.strikes`,1);
             msg.reply(`${msg.author.username} now has ${db.get(`${msg.author.id}.strikes`)} strikes!`)
-            db.set(`${msg.author.id}.messages`, arr.slice(0,arr.length-forbiddenWords[i].length))
-            //console.log(db.get(`${msg.author.id}.msgArray`))
-            //msg.delete();
-            //console.log(allMessages)
-            //console.log('Deleted message due to forbidden word');
-      //      msg.author.send("Hey, you used a bad word in your recent message. It was deleted and you were given a strike. "
-      //      +"You now have "+`${db.get(`${msg.author.id}.strikes`)} strikes!`);
-            // delete message, log, etc.
+            db.set(`${msg.author.id}.messages`, [])
             
             break;
-          }
+         }
           
         }
-        // let strikeAmount = db.get(`${msg.author!.id}.strikes`);
-        //   if(strikeAmount>5){
-        //     var role: any = msg.guild!.roles.cache.find(role => role.name == 'mute');
-        //     console.log(typeof role)
-        //     msg.guild?.members.fetch(msg.author!.id).then(user=>{
-        //         user.roles.add(role)
-        //     })
-        //   }
+        let strikeAmount = db.get(`${msg.author!.id}.strikes`);
+          if(strikeAmount>=3){
+            var role: any = msg.guild!.roles.cache.find(role => role.name == 'Mute');
+            //console.log(msg.guild!.roles.cache.find(role => role.name == 'Mute'))
+            if(msg.member!.roles.cache.some((role:any) => role.name === 'Student')){
+                msg.guild?.members.fetch(msg.author!.id).then(user=>{
+                user.roles.set([role])
+            })
+            }
+            
+          }
                 
     
                 
