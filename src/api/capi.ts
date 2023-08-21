710d763db5f90d889c8fc90f82264c76cf1d8948
Eric Zou <ezou626@gmail.com>
2023-08-21 01:19:17 -0400

    cleaned command api

--- src/api/capi.ts
+++ src/api/capi.ts
@@ -1,13 +1,14 @@
+/**
+ * Interface for command
+ */
 import * as Discord from "discord.js";
 
-export interface IBotInteraction { //basically the blueprint for all interactions to follow!
+export interface IBotInteraction {
     name(): string;
     help(): string;
     cooldown(): number;
-    isThisInteraction(command: string): boolean; //make sure commands are all checked (i could get rid of this but its a nice safeguard along with config.ts!)
-    runCommand(interaction: Discord.CommandInteraction, Bot: Discord.Client): Promise<void>; /*give the args, int and bot, and is of type Promise<void> 
-    which all that really means is that it's expecting the data to be asynchronous; not orderly. Basically it'll be a lot more lenient with you ;D
-    */
-   data(): any // return data of slash command so json can be applied
-   perms(): 'teacher' | 'student' | 'both' // add sufficient perms!
+    isThisInteraction(command: string): boolean;
+    runCommand(interaction: Discord.CommandInteraction, Bot: Discord.Client): Promise<void>;
+    data(): any
+    perms(): 'teacher' | 'student' | 'both'
 }
