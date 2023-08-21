b6e8dbcc01c47248454d1ef10c47105be50d90eb
Eric Zou <ezou626@gmail.com>
2023-08-21 01:56:15 -0400

    cleaned event api

--- src/api/eapi.ts
+++ src/api/eapi.ts
@@ -1,9 +1,10 @@
+/**
+ * Interface for event
+ */
 import * as Discord from "discord.js";
 
-export interface IBotEvent { //basically the blueprint for all events to follow!
+export interface IBotEvent {
     name(): string;
     help(): string;
-    runEvent(msg: Discord.Message, Bot: Discord.Client): Promise<void>; /*give msg and bot, and is of type Promise<void> 
-    which all that really means is that it's expecting the data to be asynchronous; not orderly. Basically it'll be a lot more lenient with you ;D
-    */
+    runEvent(msg: Discord.Message, Bot: Discord.Client): Promise<void>;
 }
