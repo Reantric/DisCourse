731ada25761390afafbdd43daa188bc42065daba
Eric Zou <ezou626@gmail.com>
2023-08-22 11:34:27 -0400

    fixed profile and refactored embed format

--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -1,4 +1,5 @@
-import * as Discord from "discord.js";
+import { Client, Role, Interaction, CommandInteraction, User, RoleManager, GuildMemberRoleManager } from "discord.js";
+import { EmbedBuilder } from "discord.js";
 import { IBotInteraction } from "../api/capi";
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { QuickDB } = require("quick.db");
@@ -25,40 +26,63 @@ export default class profile implements IBotInteraction {
         return new SlashCommandBuilder()
 		.setName(this.name())
 		.setDescription(this.help())
-        .addUserOption((option:any) => option.setName('target').setDescription('Select a user').setRequired(true));
+        .addUserOption((option:any) => option.setName('target').setDescription('Select a user'));
     }
     perms(): "teacher" | "student" | "both" {
         return 'both';
-     }
+    }
 
-    async runCommand(interaction: any, Bot: Discord.Client): Promise<void> {
-        const user = interaction.options.getUser('target');
-        let embed = new Discord.MessageEmbed();
-        if (interaction.member!.roles?.cache.some((role: { name: string; }) => role.name === 'Teacher')){
-            embed.setTitle(`${user.username}'s Profile`)
-            .setDescription(`Here is your info!`)
-            .setAuthor(user.username,user.avatarURL()!)
-            .setColor('RANDOM')
-            .addField(`Points`,`**${db.get(`${user.id}.points`)}**`,true)
-            .addField(`Strikes`,`**${db.get(`${user.id}.strikes`)}**`,true)
-            .addField(`Absences`,`**${db.get(`${user.id}.absences`)}**`,true)
-            .setThumbnail(user.avatarURL()!)
-            .setTimestamp(new Date())
-            .setFooter('DisCourse Profile');
-            await interaction.reply({content: `Here is ${user}'s profile`,embeds: [embed], ephemeral: true});  
+    private formatProfileEmbed(user: User) {
+        const embed = new EmbedBuilder();
+        embed.setTitle(`${user.username}'s Profile`)
+        .setDescription(`Here is ${user.username}'s info!`)
+        .setAuthor({name: user.username, iconURL: user.avatarURL()!})
+        .setColor('Random')
+        .addFields({
+            name: 'Points',
+            value: `**${db.get(`${user.id}.points`)}**`,
+            inline: true
+        })
+        .addFields({
+            name: 'Strikes',
+            value: `**${db.get(`${user.id}.strikes`)}**`,
+            inline: true
+        })
+        .addFields({
+            name: 'Absences',
+            value: `**${db.get(`${user.id}.absences`)}**`,
+            inline: true
+        })
+        .setThumbnail(user.avatarURL()!)
+        .setTimestamp(new Date())
+        .setFooter({text: 'DisCourse Profile'});
+        return embed;
+    }
+
+    async runCommand(interaction: CommandInteraction, Bot: Client): Promise<void> {
+        let user = interaction.options.getUser('target');
+        let roleManager = interaction.member!.roles as GuildMemberRoleManager;
+        if (roleManager.cache.has('Teacher')) {
+            if (!user) {
+                user = interaction.user;
+            }
+            const embed = this.formatProfileEmbed(user);
+            await interaction.reply({ 
+                content: `Here is ${user}'s profile`,
+                embeds: [embed], 
+                ephemeral: true 
+            });  
+        } else if (roleManager.cache.has('Student')) {
+            let message = (user ? 
+                "Since you're a student, you can't view other students' profiles." : 
+                'Here is your profile.');
+            user = interaction.user;
+            const embed = this.formatProfileEmbed(user);
+            await interaction.reply({
+                content: message, 
+                embeds: [embed], 
+                ephemeral: true
+            });  
         }
-        else if(interaction.member.roles.cache.some((role: { name: string; }) => role.name === 'Student')){
-            embed.setTitle(`${interaction.user.username}'s Profile`)
-            .setDescription(`Here is your info!`)
-            .setAuthor(interaction.user.username,interaction.user.avatarURL()!)
-            .setColor('RANDOM')
-            .addField(`Points`,`**${db.get(`${interaction.user.id}.points`)}**`,true)
-            .addField(`Strikes`,`**${db.get(`${interaction.user.id}.strikes`)}**`,true)
-            .addField(`Absences`,`**${db.get(`${user.id}.absences`)}**`,true)
-            .setThumbnail(interaction.user.avatarURL()!)
-            .setTimestamp(new Date())
-            .setFooter('DisCourse Profile');        
-            await interaction.reply({content: `Here is your profile. Since you're a student, you can't view other students' profiles.`, embeds: [embed], ephemeral: true});  
-        }
-       }
+    }
 }
